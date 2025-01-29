a68f234498c9d3b93535715d6061bc952658e0a8
Henry Cañizares <henrycaba471@gmail.com>
2025-01-28 19:04:19 -0500

    Se agrego pagina de loteria aleatoria

---
+++ src/assets/js/main.js
@@ -0,0 +1,171 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log('Running');
+    const generarColor = () => {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        return `rgb(${r},${g},${b})`;
+    }
+
+    const chances = document.querySelector('.chances');
+    const loteria = document.querySelector('.loteria');
+    const dobleChance = document.querySelector('.cuatro-cifras');
+    const dobleChanceT = document.querySelector('.tres-cifras');
+
+    const dataSorteos = document.querySelectorAll('.data-sorteos');
+    const sorteos = document.querySelectorAll('.sorteos');
+
+    const deleteBorders = (excludedElement) => {
+        sorteos.forEach((el) => {
+            if (el !== excludedElement) {
+                el.classList.remove('see-borders');
+            }
+        });
+    }
+    const deleteClass = (excludedElement) => {
+        dataSorteos.forEach((el) => {
+            if (el !== excludedElement) {
+                el.classList.remove('see-data');
+            }
+        });
+    };
+
+    chances.addEventListener('click', (e) => {
+        chances.classList.toggle('see-borders');
+        const dataSorteoChance = document.querySelector('.data-sorteo-chance');
+        dataSorteoChance.classList.toggle('see-data');
+        deleteClass(dataSorteoChance);
+        deleteBorders(chances);
+    });
+
+    loteria.addEventListener('click', (e) => {
+        loteria.classList.toggle('see-borders');
+        const dataSorteoLoteria = document.querySelector('.data-sorteo-loteria');
+        dataSorteoLoteria.classList.toggle('see-data');
+        deleteClass(dataSorteoLoteria);
+        deleteBorders(loteria);
+    });
+
+    dobleChance.addEventListener('click', (e) => {
+        dobleChance.classList.toggle('see-borders');
+        const dataSorteoDobleC = document.querySelector('.data-sorteo-4c');
+        dataSorteoDobleC.classList.toggle('see-data');
+        deleteClass(dataSorteoDobleC);
+        deleteBorders(dobleChance);
+    });
+
+    dobleChanceT.addEventListener('click', (e) => {
+        dobleChanceT.classList.toggle('see-borders');
+        const dataSorteoDobleC = document.querySelector('.data-sorteo-3c');
+        dataSorteoDobleC.classList.toggle('see-data');
+        deleteClass(dataSorteoDobleC);
+        deleteBorders(dobleChanceT);
+    });
+
+    const verNumero = document.querySelector('.ver-numero');
+    verNumero.addEventListener('click', () => {
+        const writeLuckNumber = document.querySelector('.number-play');
+        let luckNumber = Math.round(Math.random() * 9999);
+        luckNumber = luckNumber.toString().padStart(4, '0').split('');
+        writeLuckNumber.innerHTML = '';
+        writeLuckNumber.innerHTML = `<h4>Tú numero es:</h4>`
+        luckNumber.forEach(el => {
+            writeLuckNumber.innerHTML += `
+                                            <section class="balota" style="background: ${generarColor()};">
+                                                <article class="border-balota" style="background: ${generarColor()};">
+                                                    <span class="number">${el}</span>
+                                                </article>
+                                            </section>`
+        });
+    });
+
+    const verNumeroLoteria = document.querySelector('.ver-numero-loteria');
+    verNumeroLoteria.addEventListener('click', () => {
+        const writeLuckNumber = document.querySelector('.loteria-serie');
+        let luckNumber = Math.round(Math.random() * 9999);
+        luckNumber = luckNumber.toString().padStart(4, '0').split('');
+        let luckNumberSerie = Math.round(Math.random() * 999);
+        luckNumberSerie = luckNumberSerie.toString().padStart(3, '0').split('');
+        writeLuckNumber.innerHTML = '';
+        writeLuckNumber.innerHTML = `<h4>Tú numero es:</h4>`
+        luckNumber.forEach(el => {
+            writeLuckNumber.innerHTML += `
+                                            <section class="balota" style="background: ${generarColor()};">
+                                                <article class="border-balota" style="background: ${generarColor()};">
+                                                    <span class="number">${el}</span>
+                                                </article>
+                                            </section>`
+        });
+        writeLuckNumber.innerHTML += '<h1> - </h1>'
+        luckNumberSerie.forEach(el => {
+            writeLuckNumber.innerHTML += `
+                                            <section class="balota" style="background: ${generarColor()};">
+                                                <article class="border-balota" style="background: ${generarColor()};">
+                                                    <span class="number">${el}</span>
+                                                </article>
+                                            </section>`
+        });
+    });
+
+    const verNumerosDC = document.querySelector('.ver-numero-dc');
+    verNumerosDC.addEventListener('click', () => {
+        const writeLuckNumber = document.querySelector('.doble-chance');
+        writeLuckNumber.innerHTML = '';
+        writeLuckNumber.innerHTML = `<h4>Tus números son:</h4>`
+
+        for (let index = 1; index < 6; index++) {
+            let luckNumber = Math.round(Math.random() * 9999);
+            luckNumber = luckNumber.toString().padStart(4, '0').split('');
+            writeLuckNumber.innerHTML += `<div class="numero-detalles-dc">
+            <section class="balota" style="background: ${generarColor()};">
+                                                <article class="border-balota" style="background: ${generarColor()};">
+                                                    <span class="number">${luckNumber[0]}</span>
+                                                </article>
+                                            </section>
+                                            <section class="balota" style="background: ${generarColor()};">
+                                                <article class="border-balota" style="background: ${generarColor()};">
+                                                    <span class="number">${luckNumber[1]}</span>
+                                                </article>
+                                            </section>
+                                            <section class="balota" style="background: ${generarColor()};">
+                                                <article class="border-balota" style="background: ${generarColor()};">
+                                                    <span class="number">${luckNumber[3]}</span>
+                                                </article>
+                                            </section>
+                                            <section class="balota" style="background: ${generarColor()};">
+                                                <article class="border-balota" style="background: ${generarColor()};">
+                                                    <span class="number">${luckNumber[2]}</span>
+                                                </article>
+                                            </section></div>`
+        }
+    });
+
+    const verNumerosDCT = document.querySelector('.ver-numero-dct');
+    verNumerosDCT.addEventListener('click', () => {
+        const writeLuckNumber = document.querySelector('.doble-chance-tres');
+        writeLuckNumber.innerHTML = '';
+        writeLuckNumber.innerHTML = `<h4>Tus números son:</h4>`
+
+        for (let index = 1; index < 6; index++) {
+            let luckNumber = Math.round(Math.random() * 999);
+            luckNumber = luckNumber.toString().padStart(3, '0').split('');
+            writeLuckNumber.innerHTML += `<div class="numero-detalles-dc">
+            <section class="balota" style="background: ${generarColor()};">
+                                                <article class="border-balota" style="background: ${generarColor()};">
+                                                    <span class="number">${luckNumber[0]}</span>
+                                                </article>
+                                            </section>
+                                            <section class="balota" style="background: ${generarColor()};">
+                                                <article class="border-balota" style="background: ${generarColor()};">
+                                                    <span class="number">${luckNumber[1]}</span>
+                                                </article>
+                                            </section>
+                                            <section class="balota" style="background: ${generarColor()};">
+                                                <article class="border-balota" style="background: ${generarColor()};">
+                                                    <span class="number">${luckNumber[2]}</span>
+                                                </article>
+                                            </section>
+                                            </div>`
+        }
+    });
+});
