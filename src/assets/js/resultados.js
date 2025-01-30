15fb15f62f52b28fa3d0029cc492753487f14259
Henry Cañizares <henrycaba471@gmail.com>
2025-01-30 18:54:08 -0500

    Se actualizo resultados

--- src/assets/js/resultados.js
+++ src/assets/js/resultados.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('cargado');
     const todosResultados = document.querySelector('.todos-resultados');
     const URL = 'https://api-resultadosloterias.com/api/results';
     const URL_FECHA = 'https://api-resultadosloterias.com/api/results/';
@@ -12,14 +11,31 @@ document.addEventListener('DOMContentLoaded', () => {
             return response.json();
         })
         .then(data => {
-            //console.log('Datos recibidos:', data.data);
+            todosResultados.innerHTML = '';
+            data.data.sort((a, b) => {
+                if (a.lottery < b.lottery) return -1; // a viene antes que b
+                if (a.lottery > b.lottery) return 1;  // b viene antes que a
+                return 0; // son iguales
+            });
+
             data.data.forEach((item) => {
-                todosResultados.innerHTML += `<section class="resultado-loteria">
-                <div class="loteria-name"><h3>${item.lottery}</h3></div>
-                <div class="resultado"><h2>${item.result}</h2></div>
-                <div class="fecha">${item.date}</div>
-            </section>`;
-            })
+                let contenido = `
+                    <section class="resultado-loteria">
+                        <div class="loteria-name"><h3>${item.lottery}</h3></div>
+                        <div class="resultado"><h2>${item.result}</h2></div>`;
+
+                if (item.series !== '000' && item.series !== '') {
+                    contenido += `<div><span>${item.series}</span></div>`;
+                } else {
+                    contenido += `<div><span>---</span></div>`;
+                }
+
+                contenido += `
+                        <div class="fecha">${item.date}</div>
+                    </section>`;
+
+                todosResultados.innerHTML += contenido;
+            });
         })
         .catch(error => {
             console.error('Error:', error);
@@ -27,12 +43,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const btnSearch = document.getElementById('buscar-results');
     btnSearch.addEventListener('click', () => {
+        const fechaActual = new Date();
         const fechaToSearch = document.getElementById('fecha');
-        todosResultados.innerHTML = '';
+        let fechaBuscar = new Date(fechaToSearch.value);
+
         if (!fechaToSearch.value) {
             return alert('Seleccione una fecha para buscar');
         }
-        console.log(fechaToSearch.value);
+
+        if (fechaBuscar > fechaActual) {
+            todosResultados.innerHTML = '';
+            return todosResultados.innerHTML = '<div><h1 style="color: #444;">Resultados no disponibles para fechas futuras.</h1></div>'
+        }
+        //console.log(fechaToSearch.value);
 
         fetch(`${URL_FECHA}${fechaToSearch.value}`)
             .then(response => {
@@ -42,17 +65,31 @@ document.addEventListener('DOMContentLoaded', () => {
                 return response.json();
             })
             .then(data => {
-                //console.log('Datos recibidos:', data.data);
+                todosResultados.innerHTML = '';
+
+                data.data.sort((a, b) => {
+                    if (a.lottery < b.lottery) return -1; // a viene antes que b
+                    if (a.lottery > b.lottery) return 1;  // b viene antes que a
+                    return 0; // son iguales
+                });
                 data.data.forEach((item) => {
-                    todosResultados.innerHTML += `<section class="resultado-loteria">
-                <div class="loteria-name"><h3>${item.lottery}</h3></div>
-                <div class="resultado"><h2>${item.result}</h2></div>
-                <div class="fecha">${item.date}</div>
-            </section>`;
-                })
+                    let contenido = `
+                        <section class="resultado-loteria">
+                            <div class="loteria-name"><h3>${item.lottery}</h3></div>
+                            <div class="resultado"><h2>${item.result}</h2></div>`;
+                    if (item.series !== '000' && item.series !== '') {
+                        contenido += `<div><span>${item.series}</span></div>`;
+                    } else {
+                        contenido += `<div><span>---</span></div>`;
+                    }
+                    contenido += `
+                            <div class="fecha">${item.date}</div>
+                        </section>`;
+                    todosResultados.innerHTML += contenido;
+                });
             })
             .catch(error => {
                 console.error('Error:', error); // Manejar errores de red o de la API
             });
-    })
+    });
 });
